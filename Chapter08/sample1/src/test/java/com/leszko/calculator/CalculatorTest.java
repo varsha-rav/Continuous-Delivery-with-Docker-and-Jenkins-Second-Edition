16a8fcd078ef249dad45af6690aa3bd54178ac32
Varsha Ravindra <59762861+varsha-rav@users.noreply.github.com>
2026-06-11 12:37:38 -0400

    Added javadoc comment

--- Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
+++ Chapter08/sample1/src/test/java/com/leszko/calculator/CalculatorTest.java
@@ -2,6 +2,7 @@ package com.leszko.calculator;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
+/** Test for calculator feature */
 public class CalculatorTest {
      private Calculator calculator = new Calculator();
 
